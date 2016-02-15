555c76d63fb6c54cc33e26f75886edee6d94500d
Michael Lor and torrence stratton <michael.vang.1985@gmail.com>
2016-02-15 11:55:07 -0800

    add display functionality for twig html page

--- app/app.php
+++ app/app.php
@@ -15,11 +15,8 @@
 
     $app->get("/pingpong", function() use ($app) {
         $input_number = $_GET['number'];
-        echo $input_number;
-        $my_Pingpong = new PingpongGenerator($input_number);
-
-        echo $my_Pingpong->makePingPong($input_number);
-        return $app['twig']->render('pingpong.html.twig', array('return' => $my_Pingpong)) ;
+        $my_Pingpong = makePingPong($input_number);
+        return $app['twig']->render('pingpong.html.twig', array('returns' => $my_Pingpong)) ;
     });
 
     return $app;
